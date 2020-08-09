396cad403f59965c9d4145af101a568084dd29b9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-08-09 23:51:52 +0800

    Update poster.py

--- poster.py
+++ poster.py
@@ -78,7 +78,7 @@ while i > 0:
     i-=1
 
 background = actualresult
-foreground = Image.open("Copy of Arts Week-8.png")
+foreground = Image.open("overlay.png")
 
 background.paste(foreground, (0, 0), foreground)
 background.show()
